c5ac61415b6b28faedbefe5da0dfeaed93e1eb8b
francesca-marino <francescamarino112@gmail.com>
2024-06-20 12:35:40 +0200

    minor adjustment to import functions

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,6 @@
-#' Get gene symbols 
+#' Get gene symbols
 #'
-#' This function is used to get the human gene symbols from the gene Ensembl 
+#' This function is used to get the human gene symbols from the gene Ensembl
 #' IDs in the input data frame.
 #'
 #' @usage getGeneSymbols(dataf, organism = "")
@@ -11,33 +11,33 @@
 #'
 #' # Load the example data frame
 #' data(network_df)
-#' 
+#'
 #' # Get the gene symbols corresponding to the gene IDs in the data frame
 #' gene_symbols <- getGeneSymbols(network_df, organism = "hsapiens")
-#' 
-#' @import gprofiler2
+#'
+#' @importFrom gprofiler2 gconvert gorth
 #' @export
 
 getGeneSymbols <- function(dataf, organism = "") {
-  
+
   all_genes <- unique(c(dataf$from, dataf$to))
   if (organism == "hsapiens") {
-    gene_symbols <- gconvert(all_genes, organism = "hsapiens", 
+    gene_symbols <- gconvert(all_genes, organism = "hsapiens",
                              target="ENTREZGENE", filter_na = F)
   }
-  
+
   if (organism == "mmusculus") {
-    gene_symbols <- gorth(query = all_genes, source_organism = "mmusculus", 
+    gene_symbols <- gorth(query = all_genes, source_organism = "mmusculus",
                           target_organism = "hsapiens", filter_na = F)
   }
-  
+
   gene_symbols
-  
+
 }
 
 #' Convert gene Ensembl IDs in the corresponding gene symbols
 #'
-#' This function is used to convert the gene IDs of the data frame in the 
+#' This function is used to convert the gene IDs of the data frame in the
 #' corresponding gene symbols.
 #'
 #' @usage getGeneSymbolDataframe(dataf, symbols, organism = "")
@@ -46,22 +46,21 @@ getGeneSymbols <- function(dataf, organism = "") {
 #' @param organism hsapiens or mmusculus
 #' @return data frame
 #' @examples
-#' 
+#'
 #' # Load the example data frame
 #' data(network_df)
-#' 
+#'
 #' # Get the gene symbols corresponding to the gene IDs in the data frame
 #' gene_symbols <- getGeneSymbols(network_df, organism = "hsapiens")
-#' 
+#'
 #' # Get the converted data frame
-#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols, 
+#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols,
 #' organism = "hsapiens")
-#' 
-#' @import stats
+#'
 #' @export
 
 getGeneSymbolDataframe <- function(dataf, symbols, organism = "") {
-  
+
   if (organism == "hsapiens") {
     input_indices_1 <- match(dataf[, 1], symbols$input)
     input_indices_2 <- match(dataf[, 2], symbols$input)
@@ -69,9 +68,9 @@ getGeneSymbolDataframe <- function(dataf, symbols, organism = "") {
     target_genes_2 <- list(symbols$target[input_indices_2])
     dataf[, 1] <- target_genes_1
     dataf[, 2] <- target_genes_2
-    dataf <- na.omit(dataf) 
+    dataf <- na.omit(dataf)
   }
-  
+
   if (organism == "mmusculus") {
     input_indices_1 <- match(dataf[, 1], symbols$input)
     input_indices_2 <- match(dataf[, 2], symbols$input)
@@ -79,19 +78,19 @@ getGeneSymbolDataframe <- function(dataf, symbols, organism = "") {
     target_genes_2 <- list(symbols$ortholog_name[input_indices_2])
     dataf[, 1] <- target_genes_1
     dataf[, 2] <- target_genes_2
-    
+
     dataf <- na.omit(dataf)
     dataf <- dataf[-grep("N/A", dataf$from), ]
     dataf <- dataf[-grep("N/A", dataf$to), ]
   }
-  
+
   dataf
-  
+
 }
 
 #' Convert gene Ensembl IDs in the corresponding gene symbols
 #'
-#' This function is used to convert the gene IDs of the expression matrix in 
+#' This function is used to convert the gene IDs of the expression matrix in
 #' the corresponding gene symbols.
 #'
 #' @usage getGeneSymbolMatrix(vsd, symbols, organism = "")
@@ -100,24 +99,24 @@ getGeneSymbolDataframe <- function(dataf, symbols, organism = "") {
 #' @param organism hsapiens or mmusculus
 #' @return matrix to be used with VIPER
 #' @examples
-#' 
+#'
 #' # Load the example data frame
 #' data(network_df)
-#' 
+#'
 #' # Load the example matrix
 #' data(vsd_matrix)
-#' 
+#'
 #' # Get the gene symbols corresponding to the gene IDs in the data frame
 #' gene_symbols <- getGeneSymbols(network_df, organism = "hsapiens")
-#' 
+#'
 #' # Get the converted matrix
-#' converted_matrix <- getGeneSymbolMatrix(vsd_matrix, gene_symbols, 
+#' converted_matrix <- getGeneSymbolMatrix(vsd_matrix, gene_symbols,
 #' organism = "hsapiens")
-#' 
+#'
 #' @export
 
 getGeneSymbolMatrix <- function(vsd, symbols, organism = "") {
-  
+
   if (organism == "hsapiens") {
     for (i in 1:nrow(vsd)) {
       if (row.names(vsd)[i] %in% symbols$input) {
@@ -135,14 +134,14 @@ getGeneSymbolMatrix <- function(vsd, symbols, organism = "") {
         }
       }
     }
-    
+
     vsd <- vsd[-grep("^ENSG", row.names(vsd)), ]
   }
-  
+
   if (organism == "mmusculus") {
     for (i in 1:nrow(vsd)) {
       if (row.names(vsd)[i] %in% symbols$input) {
-        newname <- symbols$ortholog_name[which(symbols$input 
+        newname <- symbols$ortholog_name[which(symbols$input
                                                == row.names(vsd)[i])][1]
         if (is.na(newname) == FALSE) {
           if (!(newname %in% row.names(vsd))) {
@@ -159,19 +158,19 @@ getGeneSymbolMatrix <- function(vsd, symbols, organism = "") {
     }
     vsd <- vsd[-grep("N/A", row.names(vsd)), ]
   }
-  
+
   mat <- as.matrix(vsd)
   mat
-  
+
 }
 
 #' Get a list of genes with their interactors
 #'
-#' This function extracts from the data frame of regulators the list of the 
+#' This function extracts from the data frame of regulators the list of the
 #' gene symbols, and it adds the genes for which we want to compute the
 #' activity with VIPER.
-#' Then it filters the data frame obtained by the network to consider only the 
-#' regulators of the gene of interest and, for each regulator, it gets a list of 
+#' Then it filters the data frame obtained by the network to consider only the
+#' regulators of the gene of interest and, for each regulator, it gets a list of
 #' their interactors and the corresponding scores.
 #'
 #' @usage getInteractorsList(dataf, reg_dataf, target_genes)
@@ -180,58 +179,58 @@ getGeneSymbolMatrix <- function(vsd, symbols, organism = "") {
 #' @param target_genes list of the genes of interest
 #' @return list
 #' @examples
-#' 
+#'
 #' # Load the list of regulators
 #' data(regulators_list)
-#' 
+#'
 #' # Load the example data frame
 #' data(network_df)
-#' 
+#'
 #' # Get the gene symbols corresponding to the gene IDs in the data frame
 #' gene_symbols <- getGeneSymbols(network_df, organism = "hsapiens")
-#' 
+#'
 #' # Get the converted data frame
-#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols, 
+#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols,
 #' organism = "hsapiens")
-#' 
+#'
 #' # Get the list of the regulators with their interactors
 #' genes <- c("ADAR", "ADARB1")
 #' interactors <- getInteractorsList(converted_df, regulators_list, genes)
-#' 
-#' @import dplyr
+#'
+#' @importFrom dplyr filter
 #' @export
 
 getInteractorsList <- function(dataf, reg_dataf, target_genes) {
-  
+
   genes_selected <- reg_dataf[, 3]
   genes_selected <- c(genes_selected, target_genes)
-  
+
   df_new <- data.frame()
   for (g in genes_selected) {
-    df1 <- dataf %>% dplyr::filter(dataf[, 1] == g)
-    df2 <- dataf %>% dplyr::filter(dataf[, 2] == g)
+    df1 <- filter(dataf, dataf[, 1] == g)
+    df2 <- filter(dataf, dataf[, 2] == g)
     df2 <- df2[, c(2, 1, 3)]
     colnames(df2) <- c("from", "to", "weight")
     df3 <- rbind(df1, df2)
     df_new <- rbind(df_new, df3)
   }
-  
+
   df_list <- split(df_new, df_new$from)
   df_list <- lapply(df_list, function(dataf) {dataf[, -1]})
-  
+
   lengths <- c()
   for (tg in target_genes) {
     lengths <- c(lengths, dim(df_list[[tg]])[1])
   }
-  
+
   min_length <- min(lengths)
-  
+
   df_list <- Filter(function(dataf) nrow(dataf) >= min_length, df_list)
   df_list
-  
+
 }
 
-#' Get the adjacency matrix file as requested by VIPER 
+#' Get the adjacency matrix file as requested by VIPER
 #'
 #' This function is used to obtain the adjacency matrix file in the format
 #' requested by VIPER to obtain the regulon with the "aracne2regulon" function.
@@ -239,56 +238,56 @@ getInteractorsList <- function(dataf, reg_dataf, target_genes) {
 #' @usage writeAdjFile(df_list, file_name = "")
 #' @param df_list list of genes with their interactors
 #' @param file_name how to nominate the file
-#' @return .adj file 
+#' @return .adj file
 #' @examples
 #'
 #' # Load the list of regulators
 #' data(regulators_list)
-#' 
+#'
 #' # Load the example data frame
 #' data(network_df)
-#' 
+#'
 #' # Get the gene symbols corresponding to the gene IDs in the data frame
 #' gene_symbols <- getGeneSymbols(network_df, organism = "hsapiens")
-#' 
+#'
 #' # Get the converted data frame
-#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols, 
+#' converted_df <- getGeneSymbolDataframe(network_df, gene_symbols,
 #' organism = "hsapiens")
-#' 
+#'
 #' # Get the list of the regulators with their interactors
 #' genes <- c("ADAR", "ADARB1")
 #' interactors <- getInteractorsList(converted_df, regulators_list, genes)
 #'
 #' writeAdjFile(interactors, file_name = "interactors")
-#' 
+#'
 #' @import utils
 #' @export
 
 writeAdjFile <- function(df_list, file_name = "") {
-  
+
   file_txt <- file.path(getwd(), paste0(file_name, ".txt"))
   file_adj <- file.path(getwd(), paste0(file_name, ".adj"))
-  
+
   if (file.exists(file_txt)) {
     file.remove(file_txt)
   }
-  
+
   if (file.exists(file_adj)) {
     file.remove(file_adj)
   }
-  
+
   i = 1
   file(file_txt, "w")
   while (i < length(df_list)) {
     a <- c(names(df_list[i]), paste(df_list[[i]]$to, df_list[[i]]$weight))
-    b <- write.table(x = rbind(a), file = file_txt, row.names = F, 
+    b <- write.table(x = rbind(a), file = file_txt, row.names = F,
                      col.names = F, quote = F, append = T)
     i = i+1
-  } 
-  
+  }
+
   txt_file <- readLines(file_txt)
   adj_file <- file_adj
   modified_file <- gsub(" ", "\t", txt_file)
   writeLines(modified_file, adj_file)
-  
+
 }
